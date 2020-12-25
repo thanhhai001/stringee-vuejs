c5a587d638d4cdc0383d6e12550834fbf3e30858
thanhhai001 <haicnt.iuh@gmail.com>
2020-12-26 04:50:11 +0700

    change database

--- src/util/process/api.js
+++ src/util/process/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export default () => {
   return axios.create({
-    baseURL: 'http://localhost:3005', // local host
-    //server...
+    // baseURL: 'http://localhost:3005', // local host
+    baseURL: 'ec2-52-44-235-121.compute-1.amazonaws.com' //server...
   })
 }
